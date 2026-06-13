a36f4c50f72a1ab8508aa0b38f6ac36b64ecb8dc
hollow <velpointl2@gmail.com>
2026-06-13 18:39:10 -0300

    feat: solve Leetcode 1 - Two Sum

--- leetcode/arrays-and-hashing/two-sum.py
+++ leetcode/arrays-and-hashing/two-sum.py
@@ -1,7 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int):
-        for i in range(len(nums)):
-            for x in range(i+1,len(nums)):
-                if nums[i]+nums[x] == target:
-                    return [i, x]
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
         return None
