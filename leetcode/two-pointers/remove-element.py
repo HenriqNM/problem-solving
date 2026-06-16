7a37da6e0b8448fd7207c1cdfdbcf958993a2f53
hollow <velpointl2@gmail.com>
2026-06-15 23:27:09 -0300

    feat: solve Leetcode 27 - Remove Element

--- leetcode/two-pointers/remove-element.py
+++ leetcode/two-pointers/remove-element.py
@@ -1,4 +1,12 @@
+from typing import List
+
 class Solution:
-    def removeElement(self, nums: list, val: int):
-        nums[:] = [n for n in nums if n != val]
-        return sum(1 for n in nums if isinstance(n, int))
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
